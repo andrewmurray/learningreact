6c482ac69516f81a4fc6ce1f236379e3c1f43373
Andrew Murray <sckoobs@gmail.com>
2017-01-28 21:13:27 +0000

    Initial commit

---
+++ js/ClientApp.js
@@ -0,0 +1,30 @@
+/* global React ReactDOM */
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+         h1(null, this.props.title)
+      )
+    )
+  }
+})
+
+var MyTitleFact = React.createFactory(MyTitle)
+
+var MyFirstComponent = React.createClass({
+  render () {
+    return (
+      div(null,
+        MyTitleFact({title: 'Props are great!'}),
+        MyTitleFact({title: 'Use props everywhere!'}),
+        MyTitleFact({title: 'Props are the best!'})
+      )
+    )
+  }
+})
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
