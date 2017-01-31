066b09b36da1589d896f9cc6ada7dd25b7674181
Andrew Murray <sckoobs@gmail.com>
2017-01-31 21:50:26 +0000

    Converted ClientApp.js over to JSX

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,19 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div
-
-var MyTitleFact = React.createFactory(MyTitle)
-
 var MyFirstComponent = React.createClass({
   render () {
     return (
-      div(null,
-        MyTitleFact({title: 'this one here', color: 'indigo'}),
-        MyTitleFact({title: 'and another', color: 'peru'}),
-        MyTitleFact({title: 'and some more', color: 'MediumSpringGreen'}),
-        MyTitleFact({title: 'now the last time', color: 'OrangeRed'})         
-      )
+      <div>
+        <MyTitle title='this one here' color='indigo' />
+        <MyTitle title='and another' color='peru' />
+        <MyTitle title='and some more' color='MediumSpringGreen' />
+        <MyTitle title='now the last time' color='OrangeRed' />
+      </div>
     )
   }
 })
