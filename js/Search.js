c5dd41c5bac247e0da629b312309bcb7878148ea
Andrew Murray <sckoobs@gmail.com>
2017-02-20 07:12:10 +0000

    Added ShowCard

--- js/Search.js
+++ js/Search.js
@@ -1,11 +1,14 @@
 import React from 'react';
+
+import ShowCard from './Showcard';
+
 import preload from '../data/movies.json';
 
 const Search = React.createClass({
   render () {
     return (
       <div className=''>
-        {preload.shows.map(show => <h3>{show.title}</h3>)}
+        {preload.shows.map(show => <ShowCard key={show.mdbID} show={show} />)}
       </div>
     );
   }
