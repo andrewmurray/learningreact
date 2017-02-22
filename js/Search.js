4788e1ee2ec488134b221a3fab75bc72fa4297d4
Andrew Murray <sckoobs@gmail.com>
2017-02-22 13:00:39 +0000

    Added header to search page with search box and onChange handler

--- js/Search.js
+++ js/Search.js
@@ -5,10 +5,24 @@ import ShowCard from './Showcard';
 import preload from '../data/movies.json';
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    };
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value});
+  },
   render () {
     return (
       <div className=''>
-        {preload.shows.map(show => <ShowCard key={show.mdbID} show={show} />)}
+        <header>
+          <h1>vidflix</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} placeholder='Search' type='text' />
+        </header>
+        {preload.shows
+          .filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()))
+          .map(show => <ShowCard key={show.mdbID} show={show} />)}
       </div>
     );
   }
