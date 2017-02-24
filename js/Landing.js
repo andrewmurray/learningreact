aeb96190285867c625ed56af3e1e90fcb816cf1a
Andrew Murray <sckoobs@gmail.com>
2017-02-24 23:20:02 +0000

    Doing some more styling

--- js/Landing.js
+++ js/Landing.js
@@ -5,15 +5,13 @@ import {Link} from 'react-router-dom';
 const Landing = React.createClass({
   render () {
     return (
-      <div className='landing-page'>
-        <div className='landing-panel'>
-          <h1>vidflix</h1>
-          <div className='form-group'>
-            <label htmlFor='email'>Search</label>
-            <input type='email' className='form-control' id='email' placeholder='Search' />
-          </div>
-          <Link to='/search'>or browse all</Link>
+      <div className='landing-panel'>
+        <h1>vidflix</h1>
+        <div className='form-group'>
+          <label htmlFor='email'>Search</label>
+          <input type='email' className='form-control' id='email' placeholder='Search' />
         </div>
+        <Link to='/search'>or browse all</Link>
       </div>
     );
   }
